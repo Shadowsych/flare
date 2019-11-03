93e3454cbc60ad9a7c1a41656b69ff187180ad80
Shadowsych <pravat.bhusal@gmail.com>
2019-11-02 23:27:24 -0700

    Set-up the Stack navigator using React Navigation

--- App.js
+++ App.js
@@ -1,122 +1,43 @@
-import React, {Component} from 'react';
-import {
-    AppRegistry,
-    StyleSheet,
-    Text,
-    View,
-    Image,
-    StatusBar,
-    Button,
-    Alert,
-    Navigator} from 'react-native';
+import {AppRegistry, Platform, StatusBar} from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from "react-navigation-stack";
+import {fromBottom, fromTop, fromLeft, zoomIn} from 'react-navigation-transitions';
 
-import {LinearGradient} from 'expo-linear-gradient';
-import CameraButton from './components/CameraButton';
-import InfoButton from './components/InfoButton';
-import PopulationMap from './components/PopulationMap';
+// screens
+import Map from "./screens/Map";
+import Info from "./screens/Info";
 
-export default class App extends Component {
-    render = () => {
-        return (
-            <Navigator initialRoute = { (id: 'HomePage') }>
+// custom animation transitions
+const handleCustomTransition = ({ scenes }) => {
+  const prevScene = scenes[scenes.length - 2];
+  const nextScene = scenes[scenes.length - 1];
 
-
-            /*Primary Page*/
-            <View style = {sectionStyles.container}>
-                /*Hide Mobile Status Bar*/
-                <StatusBar hidden={1} />
-                /*Header which contains app title*/
-                <View style = {sectionStyles.header}>
-                    <Image
-                        style = {elementStyles.title}
-                        source = {require('../Flare/assets/titleicon.png')}
-                    />
-                </View>
-                /*Body section containing map component*/
-                <View className = 'body' style = {sectionStyles.body}>
-
-
-                <PopulationMap/>
-
-
-                </View>
-                /*Footer containing navigation tab to camera and info*/
-                <LinearGradient
-                        colors={[UI_THEME_COLOR_ONE, UI_THEME_COLOR_TWO]}
-                        start={{ x: 0, y: .95 }}
-                        end={{ x: 0, y: .3 }}
-                        style = {sectionStyles.footer}>
-                    /*Navigation*/
-                    <View style = {elementStyles.button}>
-                        <InfoButton/>
-                    </View>
-                    <View style = {elementStyles.button}>
-                        <CameraButton/>
-                    </View>
-                </LinearGradient>
-
-            </View>
-        );
-    }
+  if(prevScene &&
+    prevScene.route.routeName == "Map"
+    && nextScene.route.routeName == "Info") {
+      return fromBottom();
+  }
+  // by default, open the page from the left
+  return fromLeft();
 }
 
-const UI_THEME_COLOR_ONE = '#ff425e';
-const UI_THEME_COLOR_TWO = 'rgba(255,66,94,0)';
-const UI_THEME_FONT_ONE = 'Verdana';
-
-const UI_THEME_TITLE_SIZE = '59%';
-
-const elementStyles = StyleSheet.create({
-    title: {
-        resizeMode: 'contain',
-        height: UI_THEME_TITLE_SIZE,
-        top: 10,
-    },
-    button: {
-        width: 50,
-        height: 50,
-        top: 35,
-        margin: 20,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+// create the stackNavigator that holds all the pages (Intents)
+const Navigation = createStackNavigator({
+  Map: {
+    screen: Map
+  },
+	Info: {
+		screen: Info
+	}
+}, {
+  // default navigation options
+  defaultNavigationOptions: {
+    header: null
+  },
+  // transition animations to other pages
+  transitionConfig: (nav) => handleCustomTransition(nav)
 });
 
-const sectionStyles = StyleSheet.create({
-    header: {
-        zIndex: 1,
-        top: -20,
-        position: 'absolute',
-        backgroundColor: UI_THEME_COLOR_ONE,
-        width: '65%',
-        height: '10%',
-        borderRadius: 20,
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    body: {
-        backgroundColor: 'white',
-        width: '100%',
-        height: '100%',
-
-    },
-    footer: {
-        bottom: 0,
-        width: '100%',
-        height: '20%',
-        position: 'absolute',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-    },
-    container: {
-        height: '100%',
-        flexDirection: 'column',
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
-
-AppRegistry.registerComponent('App', () => App);
+// export the App class as an app container
+const App = createAppContainer(Navigation);
+export default App;
--- components/PopulationMap.js
+++ components/PopulationMap.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 import MapView from 'react-native-maps';
 import {
+    Alert,
     AppRegistry,
     StyleSheet,
     View,
     Image,
     Dimensions} from 'react-native';
+
+import * as Location from "expo-location";
+import * as Permissions from "expo-permissions";
 
 import {
     Appearance,
@@ -15,24 +19,84 @@ import {
 
 export default class PopulationMap extends Component {
 
+  // construct the state of the component
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: {
+        longitude: -97.73675,
+        latitude: 30.28265
+      },
+      locationDelta: {
+        longitudeDelta: 0.005,
+        latitudeDelta: 0.005
+      },
+      markers: []
+    }
+
+    // initial mapview
+    this.mapView = undefined;
+  }
+
+    // called whenever the component loads
+   componentDidMount() {
+     this.watchLocation();
+   }
+
     render() {
         return (
             <View>
                 <AppearanceProvider>
                     <MapView
+                        ref={(mapView) => {this.mapView = mapView}}
                         style={elementStyles.mapStyle}
                         mapType = 'standard'
-                        showsUserLocation = {true}
-                        followsUserLocation = {true}
-                        maxZoomLevel = {20}
+                        initialRegion={{
+                          longitude: this.state.location.longitude,
+                          longitudeDelta: this.state.locationDelta.longitudeDelta,
+                          latitude: this.state.location.latitude,
+                          latitudeDelta: this.state.locationDelta.latitudeDelta
+                        }}
+                        showsUserLocation={true}
                     />
                 </AppearanceProvider>
             </View>
         )
     }
+
+    // watch the location of the user
+  async watchLocation() {
+    let {status} = await Permissions.askAsync(Permissions.LOCATION);
+    if (status === "granted") {
+      // continously watch the location of the user
+      Location.watchPositionAsync({enableBalancedAccuracy: true,
+        timeInterval: 3000, distanceInterval: 10}, (position) => {
+          // the transition time in miliseconds
+          let transitionTime = 1000;
+
+          // animate the camera to the region
+          this.mapView.animateCamera({
+            center: {
+              longitude: position.coords.longitude,
+              latitude: position.coords.latitude
+            }
+          }, transitionTime);
+
+          // set the state of the longitude and latitude
+          this.setState({
+            location: {
+              longitude: position.coords.longitude,
+              latitude: position.coords.latitude
+            }
+          });
+      }).then((resolved) => {
+        return;
+      }).catch((error) => {
+        Alert.alert("GPS Error!", "Please make sure your location (GPS) is turned on.");
+      });
+    }
+  }
 }
-
-
 
 const elementStyles = StyleSheet.create({
     mapStyle: {
