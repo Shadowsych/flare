966b08b2a53ac91a4c5083d30db63acde0cbbc8a
Shadowsych <pravat.bhusal@gmail.com>
2019-11-03 00:50:17 -0700

    Finish programming the image detection from the client to server

--- server/server.py
+++ server/server.py
@@ -15,7 +15,7 @@ from tensorflow.keras.preprocessing import image
 import base64
 
 # load the session and model's h5 file
-sess = tf.Session(config=tf_config)
+sess = tf.Session()
 graph = tf.get_default_graph()
 set_session(sess)
 classifier = load_model("../notebooks/model.h5")
@@ -29,7 +29,7 @@ def predict_density():
     # format the base 64 image from the client
     picture_base_64 = request.form.get("pictureBase64")
     picture_base_64 += '=' * (-len(picture_base_64) % 4)
-    
+
     # save the image into this directory
     image_file = "capture.jpg"
     with open(image_file, "wb") as fh:
@@ -37,7 +37,7 @@ def predict_density():
 
     # function call to predict the level of people within the area
     prediction = predict(image_file, 224, 224)
-    
+
     # set the prediction in JSON format
     prediction = jsonify(prediction)
 
@@ -47,23 +47,23 @@ def predict_density():
 def predict(image_file, img_width, img_height):
     global sess
     global graph
-    
+
     # reformats image for Numpy Array conversion
     predict_image = image.load_img(
         image_file, target_size=(img_width, img_height)
     )
-    
+
     # converts the image to an array
     image_matrix = image.img_to_array(predict_image)
-    
+
     # expands the shape of the array
     image_matrix = np.expand_dims(image_matrix, axis= 0)
-    
+
     # classifies the level of people within the image
     with graph.as_default():
         set_session(sess)
         result = classifier.predict(image_matrix)
-    
+
     # returns best category classification
     categories = {0: "High", 1: "Low", 2: "Medium", 3: "Zero"}
     return get_predicted_population_density(result, categories)
@@ -74,12 +74,11 @@ def get_predicted_population_density(predictions, categories):
 
     # receive the category with the highest predicted possibility
     while(current_category < len(predictions[0])):
-        print(predictions[0][current_category])
         if(predictions[0][current_category] > predictions[0][best_category]):
             # update the best category
             best_category = current_category
         current_category += 1
-        
+
     return categories[best_category]
 
 app.run(host= "0.0.0.0", port= "5000")
