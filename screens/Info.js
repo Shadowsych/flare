f4bf5c16b97a3bf7d2574edbddc15eb254421aa1
Fronrich Puno <fronrich@utexas.edu>
2019-11-03 04:46:36 -0600

    UI Overhaul

--- screens/Info.js
+++ screens/Info.js
@@ -13,6 +13,45 @@ export default class Info extends Component {
     render = () => {
         return (
             <View style = {sectionStyles.container}>
+                <View style = {sectionStyles.header}>
+                    <Text style = {elementStyles.title}>
+                        Victim Density
+                    </Text>
+                </View>
+
+                <View style = {elementStyles.helpBox}>
+                    <Image
+                        style = {elementStyles.helpImage}
+                        source = {require('../assets/green.png')}
+                    />
+                    <Text style = {elementStyles.helpText}>
+                        - Low Density (1-5 Victims)
+                    </Text>
+                </View>
+                <View style = {elementStyles.helpBox}>
+                    <Image
+                        style = {elementStyles.helpImage}
+                        source = {require('../assets/orange.png')}
+                    />
+                    <Text style = {elementStyles.helpText}>
+                        - Medium Density (6-30 Victims)
+                    </Text>
+                </View>
+                <View style = {elementStyles.helpBox}>
+                    <Image
+                        style = {elementStyles.helpImage}
+                        source = {require('../assets/red.png')}
+                    />
+                    <Text style = {elementStyles.helpText}>
+                        - High Density (More Than 31 Victims)
+                    </Text>
+                </View>
+
+                <View style = {sectionStyles.footer}>
+                    <Text style = {elementStyles.title}>
+                        SWIPE RIGHT TO RETURN >>>
+                    </Text>
+                </View>
             </View>
         );
     }
@@ -25,52 +64,61 @@ const UI_THEME_FONT_ONE = 'Verdana';
 const UI_THEME_TITLE_SIZE = '59%';
 
 const elementStyles = StyleSheet.create({
-    title: {
-        resizeMode: 'contain',
-        height: UI_THEME_TITLE_SIZE,
-        top: 10,
-    },
-    button: {
-        width: 50,
-        height: 50,
-        top: 35,
-        margin: 20,
+    helpBox: {
+        width: '95%',
+        height: 70,
+        backgroundColor: '#eee',
+        borderRadius: 20,
+        margin: 25,
+        flexDirection: 'row',
         alignItems: 'center',
-        justifyContent: 'center',
+    },
+    helpImage: {
+        resizeMode: 'contain',
+        height: '100%',
+        flexBasis: '20%',
+    },
+    helpText: {
+        fontFamily: UI_THEME_FONT_ONE,
+        fontSize: 15,
+        flexBasis: '80%',
+
+    },
+    title: {
+        fontSize: 25,
+        fontWeight: 'bold',
+        fontStyle: 'italic',
+        color: 'white',
     },
 });
 
 const sectionStyles = StyleSheet.create({
     header: {
         zIndex: 1,
-        top: -20,
+        top: 0,
         position: 'absolute',
         backgroundColor: UI_THEME_COLOR_ONE,
-        width: '65%',
+        width: '100%',
         height: '10%',
-        borderRadius: 20,
         flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
-    },
-    body: {
-        backgroundColor: 'white',
-        width: '100%',
-        height: '100%',
-    },
-    footer: {
-        bottom: 0,
-        width: '100%',
-        height: '20%',
-        position: 'absolute',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
     },
     container: {
         height: '100%',
         flexDirection: 'column',
         backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    footer: {
+        zIndex: 1,
+        bottom: 0,
+        position: 'absolute',
+        backgroundColor: UI_THEME_COLOR_ONE,
+        width: '100%',
+        height: '10%',
+        flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
     },
--- screens/Map.js
+++ screens/Map.js
@@ -10,6 +10,7 @@ import {
     Button,
     Alert} from 'react-native';
 
+import { Icon } from 'react-native-elements'
 import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from "expo-permissions";
 
@@ -17,6 +18,9 @@ import {LinearGradient} from 'expo-linear-gradient';
 import CameraButton from '../components/CameraButton';
 import InfoButton from '../components/InfoButton';
 import PopulationMap from '../components/PopulationMap';
+
+// <InfoButton/>
+// <CameraButton/>
 
 export default class Map extends Component {
     render = () => {
@@ -36,21 +40,26 @@ export default class Map extends Component {
 
 
                 </View>
-                <LinearGradient
-                        colors={[UI_THEME_COLOR_ONE, UI_THEME_COLOR_TWO]}
-                        start={{ x: 0, y: .95 }}
-                        end={{ x: 0, y: .3 }}
-                        style = {sectionStyles.footer}>
+                <View style = {sectionStyles.footer}>
                     <TouchableOpacity onPress={() => this.loadInfoPage()} style = {elementStyles.button}>
-                        <InfoButton/>
+                        <Icon reverse raised type = 'font-awesome' name = 'info' color = {UI_THEME_COLOR_ONE}/>
+
+
                     </TouchableOpacity>
                     <TouchableOpacity onPress={() => this.uploadPictureDemo()} style = {elementStyles.button}>
-                        <CameraButton/>
+                        <Icon reverse raised type = 'font-awesome' name = 'camera-retro' color = {UI_THEME_COLOR_ONE}/>
+
+
                     </TouchableOpacity>
-                </LinearGradient>
+                </View>
 
             </View>
         );
+    }
+
+    // load the information page
+    loadInfoPage() {
+      this.props.navigation.navigate("Info");
     }
 
     // upload a picture demonstration
@@ -91,11 +100,6 @@ export default class Map extends Component {
         });
       }
     }
-
-    // load the information page
-    loadInfoPage() {
-      this.props.navigation.navigate("Info");
-    }
 }
 
 const UI_THEME_COLOR_ONE = '#ff425e';
@@ -113,7 +117,8 @@ const elementStyles = StyleSheet.create({
     button: {
         width: 50,
         height: 50,
-        top: 35,
+        borderRadius: 5,
+        top: -10,
         margin: 20,
         alignItems: 'center',
         justifyContent: 'center',
@@ -141,7 +146,7 @@ const sectionStyles = StyleSheet.create({
     footer: {
         bottom: 0,
         width: '100%',
-        height: '20%',
+        height: '10%',
         position: 'absolute',
         flexDirection: 'row',
         alignItems: 'center',
